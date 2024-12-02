66e672b783f57c0a3a2d29d2dd5c3296b2d3a8e1
Steven Thonus <steven.thonus@infi.nl>
2024-12-02 12:09:56 +0100

    cleanup day2

--- day2/index.ts
+++ day2/index.ts
@@ -3,24 +3,19 @@ import { TextLineStream } from "@std/text-line-stream";
 export const main = async () => {
   console.warn(`day2 start`);
   const items = await getData('input');
-  const safeItems = items.map(i => {
-    return isSafe(i);
-  }).filter(i => i == 'SAFE')
+  const safeItems = items.filter(isSafe)
   console.debug('day2.1:', safeItems.length);
 
-  const fixedItems = items.map(i => {
-    return  hasSafeAlternative(i) || isSafe(i);
-  }).filter(i => i == 'SAFE')
+  const fixedItems = items.filter(i => {
+    if (isSafe(i)) return true;
+    if (safeAlternatives(i).length > 0) return true;
+    return false;
+  })
   console.debug('day2.2:', fixedItems.length);
 };
 
-const hasSafeAlternative = (items: number[]) => {
-  // console.log('item:', items);
-  return items.map((_item, idx) => {
-    const alternatives = items.filter((_, i) => i != idx);
-    // console.log(' - ', alternatives, isSafe(alternatives));
-    return isSafe(alternatives)
-  }).filter(i => i == 'SAFE').length > 0 ? 'SAFE' : 'UNSAFE';
+const safeAlternatives = (items: number[]) => {
+  return items.filter((_item, idx) => isSafe(items.filter((_, i) => i != idx)));
 };
 
 const isOrdered = (items: number[]) => {
@@ -29,23 +24,21 @@ const isOrdered = (items: number[]) => {
 };
 
 const overMaxDistance = (sorted: number[]) => {
-  return sorted.find((cur, idx) => {
+  return !!sorted.find((cur, idx) => {
     if (idx == sorted.length - 1) return false
     const diff = Math.abs(cur - sorted[idx+1]);
     const invalidDiff = (diff < 1 || diff > 3);
-    // console.debug('maxDistance:', cur, items[idx+1], '=', diff, invalidDiff ? 'UNSAFE' : 'SAFE');
     return invalidDiff;
   });
 }
 
 const isSafe = (items: number[]) => {
   const sorted = [...items].sort((a, b) => a - b);
-  const alreadySorted = isOrdered(items);
+  const isAlreadySorted = isOrdered(items);
   const overMax = overMaxDistance(sorted);
-  // console.debug('items:', items.join(), alreadySorted && !overMaxDistance ? 'SAFE' : 'UNSAFE');
-  if (!alreadySorted) return 'UNSAFE';
-  if (overMax) return 'UNSAFE';
-  return 'SAFE';
+  if (!isAlreadySorted) return false
+  if (overMax) return false;
+  return true;
 }
 
 const getData = async (filename: string) => {
