2d17a2230ecccb33c5a1a6de3705a714845f3b5c
Steven Thonus <steven.thonus@infi.nl>
2024-12-03 16:55:08 +0100

    day 3.2

--- day3/index.ts
+++ day3/index.ts
@@ -1,22 +1,17 @@
-import { TextLineStream } from "@std/text-line-stream";
-
 const calcMul = (item: string) => {
   const nums = item.match(/([0-9]{1,3})/g);
-  // console.warn('nums', nums); 
   if (!nums) {
     throw new Error('nums is not found');
   }
   const mult = parseInt(nums[0]) * parseInt(nums[1]);
-  // console.warn('mult', mult);
   return mult
 };
 
-const getFle = async (filename: string) => {
+const getMuls = async (filename: string) => {
   const items: string[] = [];
   using f = await Deno.open(`day3/${filename}.txt`)
   const readable = f.readable
-  .pipeThrough(new TextDecoderStream())
-  .pipeThrough(new TextLineStream());
+  .pipeThrough(new TextDecoderStream());
 
   for await (const line of readable) {
     const matches = line.matchAll(/mul\([0-9]{1,3},[0-9]{1,3}\)/g);
@@ -27,11 +22,39 @@ const getFle = async (filename: string) => {
   return items;  
 }
 
+const getMuls2 = async (filename: string) => {
+  const items: string[] = [];
+  using f = await Deno.open(`day3/${filename}.txt`)
+  const readable = f.readable
+  .pipeThrough(new TextDecoderStream());
+
+  for await (const line of readable) {
+    const parts = line.split(/do\(\)/);
+
+    parts.forEach((part) => {
+      const enabledParts = part.split(/don't\(\)/);
+      const enabledPart = enabledParts[0]
+      const matches = enabledPart.matchAll(/mul\([0-9]{1,3},[0-9]{1,3}\)/g);
+      matches.forEach((match) => {
+        items.push(match[0]);
+      })
+    });
+  }
+  return items;  
+}
+
 export const main = async () => {
   console.warn(`day3.1 start`);
-  const items = await getFle('input')
+  const items = await getMuls('demo')
   const sum = items.reduce((acc, item) => {
     return acc + calcMul(item)
   }, 0);
   console.warn('day3.1 sum mults is', sum);
+
+  console.warn(`day3.2 start`);
+  const items2 = await getMuls2('input')
+  const sum2 = items2.reduce((acc, item) => {
+    return acc + calcMul(item)
+  }, 0);
+  console.warn('day3.2 sum mults is', sum2);
 };
